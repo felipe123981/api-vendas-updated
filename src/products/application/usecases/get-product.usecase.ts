beb6190a6597cda1b6ddd331899187e5cfa91ca6
felipe123981 <filipe.timachine@gmail.com>
2025-06-06 01:47:53 -0300

    getProductUseCase

---
+++ src/products/application/usecases/get-product.usecase.ts
@@ -0,0 +1,38 @@
+import { ProductsRepository } from "@/products/domain/repositories/products.repository";
+import { inject, injectable } from "tsyringe";
+
+export namespace GetProductUseCase {
+  export type Input = {
+    id: string;
+  };
+
+  export type Output = {
+    id: string;
+    name: string;
+    price: number;
+    quantity: number;
+    created_at: Date;
+    updated_at: Date;
+  };
+
+  @injectable()
+  export class UseCase {
+    constructor(
+      @inject("ProductsRepository")
+      private productRepository: ProductsRepository,
+    ) {}
+
+    async execute(input: Input): Promise<Output> {
+      const product = await this.productRepository.findById(input.id);
+
+      return {
+        id: product.id,
+        name: product.name,
+        price: product.price,
+        quantity: product.quantity,
+        created_at: product.created_at,
+        updated_at: product.updated_at,
+      };
+    }
+  }
+}
