24be60212c503aabf5e87ce0d51499359fead343
felipe123981 <filipe.timachine@gmail.com>
2025-06-18 00:45:28 -0300

    Implemented users controller and routes

--- src/common/infrastructure/http/routes/index.ts
+++ src/common/infrastructure/http/routes/index.ts
@@ -1,11 +1,15 @@
 import { productsRouter } from '@/products/http/routes/products.routes'
+import { usersRouter } from '@/users/infrastructure/http/routes/users.route'
 import { Router } from 'express'
 
 const routes = Router()
 
-routes.get('/', (req, resp) => {
-  return resp.status(200).json({ message: 'Hello Dev!' })
+routes.get('/', (req, res) => {
+  return res.status(200).json({ message: 'Olá Dev!' })
 })
+
 routes.use('/products', productsRouter)
+
+routes.use('/users', usersRouter)
 
 export { routes }
--- src/users/infrastructure/container/index.ts
+++ src/users/infrastructure/container/index.ts
@@ -1,9 +1,8 @@
-import { dataSource } from '@/common/infrastructure/typeorm'
-import { UsersTypeormRepository } from '@/users/infrastructure/typeorm/repositories/users-typeorm.repository'
 import { container } from 'tsyringe'
 import { User } from '../typeorm/entities/users.entity'
 import { CreateUserUseCase } from '@/users/application/usecases/create-user.usecase'
-import { BcryptjsHashProvider } from '@/common/infrastructure/providers/hash-provider/bcryptjs-hash.provider'
+import { UsersTypeormRepository } from '../typeorm/repositories/users-typeorm.repository'
+import { dataSource } from '@/common/infrastructure/typeorm'
 
 container.registerSingleton('UsersRepository', UsersTypeormRepository)
 container.registerInstance(
@@ -11,4 +10,3 @@ container.registerInstance(
   dataSource.getRepository(User),
 )
 container.registerSingleton('CreateUserUseCase', CreateUserUseCase.UseCase)
-container.registerSingleton('HashProvider', BcryptjsHashProvider)
